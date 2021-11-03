f51cb77115527d89caea7c42880fb5c0c47e9c87
cristopher munoz pareja <ceristh@gmail.com>
2021-11-03 09:48:06 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'Hola Cristopher'
+        }
+    },
+}
+</script>
+
+<style scoped>
+    #app {
+        background-color: blue;
+    }
+</style>
